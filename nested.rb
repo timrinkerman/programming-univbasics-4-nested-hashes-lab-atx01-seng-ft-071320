200a2690719e5d7f4459f21c351ffc457012c5e7
Tim Rinkerman <tim.rinkerman98@gmail.com>
2020-06-13 15:52:52 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,9 +81,9 @@ def adding_matz
         :languages => ["C"]
       }
     }
-   programmer_hash << {:yukihiro_matsumoto => {
+   programmer_hash.unshift(:yukihiro_matsumoto => {
    :known_for => "Ruby",
-   :languages => ["LISP", "C"]}} 
+   :languages => ["LISP", "C"]}) 
 end
 
 def changing_alan
